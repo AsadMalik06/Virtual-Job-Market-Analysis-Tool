2d4cd058749af69964c91c2c2b6463de78700d64
Muhammad Idrees <muhammadidr1122@gmail.com>
2024-12-20 10:18:23 -0800

    Commit 2

---
+++ app/Components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+   <nav className='bg-white flex justify-between items-center  h-16'>
+
+  <Link href="/">  <div className="logo flex md:gap-4 items-center">
+        <h1 className='text-3xl font-serif   text-green-900 font-bold cursor-pointer ml-5'>HireTask</h1>
+        <p className='cursor-pointer hidden md:block'>Hire or get Hired!</p>
+    </div></Link>
+
+<ul className='md:flex hidden justify-center gap-6  font-bold  items-center mr-5'>
+<Link href="/Worker">  <li className='hover:scale-105 transition-all cursor-pointer'>Skills Tracker</li></Link>
+    <Link href="/Admin"><li className='hover:scale-105 transition-all cursor-pointer'>Job Posting</li></Link>
+    <li className='hover:scale-105 transition-all cursor-pointer'>About me</li>
+    <div className='bg-green-700 mr-4 hidden md:flex  text-white py-1 px-4 rounded hover:bg-green-800 cursor-pointer'>Post Job</div>
+
+   
+</ul>
+<div className='bg-green-700 mr-4 md:hidden  text-white py-1 px-2 rounded hover:bg-green-800 cursor-pointer'>Post Job</div>
+
+</nav>
+  )
+}
+
+export default Navbar
---
+++ app/Worker/page.js
@@ -0,0 +1,25 @@
+"use client"
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+const page = () => {
+ 
+  return(
+ <>
+    
+   <div className="page">
+    <h1 className='text-center md:text-3xl font-bold text-2xl text-gray-700 mb-4 mt-10'>Skills Tracking and Job Recomendations</h1>
+
+
+<p className='text-center p-5 '>Now you can use our Artificial Intelligence based skills and job recomendation system to explore opportunities for yourself</p>
+
+   </div>
+
+    </>
+)
+    
+  
+ 
+}
+
+export default page
